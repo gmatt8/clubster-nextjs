b21ae03c7bbad60790e166ee043ed177c7ebc417
Mattia <mattia.gori@hotmail.com>
2025-04-09 23:36:28 +0200

    “update“

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css';
 import Script from 'next/script';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: 'Clubster',
